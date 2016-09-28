db50f39162791508c6a34957cf83782879734327
henrykchan <chan.henryk@gmail.com>
2016-09-28 09:17:15 -0400

    dictinonaryFun Complete

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -10,11 +10,33 @@ class Icecream {
     
     // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
     
-    
-    
+    var favoriteFlavorsOfIceCream: [String : String] = [
+        "Joe" : "Peanut Butter and Chocolate",
+        "Tim" : "Natural Vanilla",
+        "Sophie" : "Mexican Chocolate",
+        "Deniz" : "Natural Vanilla",
+        "Tom" : "Mexican Chocolate",
+        "Jim" : "Natural Vanilla",
+        "Susan" : "Cookies 'N' Cream"
+    ]
     
     
     // 2.
+    
+    func names(forFlavor flavor:String) -> [String]  {
+    
+        var nameWithSameFlavor : [String] = []
+        
+        for (eachName,eachFlavor) in favoriteFlavorsOfIceCream {
+            
+            if flavor == eachFlavor {
+                
+                nameWithSameFlavor.append(eachName)
+            }
+            
+        }
+        return (nameWithSameFlavor)
+    }
     
     
     
@@ -23,7 +45,20 @@ class Icecream {
     
     // 3.
     
-    
+    func count (forFlavor flavor:String) -> Int {
+        
+        var noLikeCount = 0
+        
+        for eachFlavor in favoriteFlavorsOfIceCream.values {
+            
+            if flavor == eachFlavor {
+                
+                noLikeCount += 1
+            }
+        }
+        
+        return noLikeCount
+    }
     
     
     
@@ -31,14 +66,44 @@ class Icecream {
     
     // 4.
    
-    
-    
-    
-    
-    
-    
+    func flavor (forPerson:String) -> String? {
+        
+        
+        
+        for (person,flavor) in favoriteFlavorsOfIceCream {
+            
+            if forPerson == person {
+                
+                return flavor
+            }
+            
+        }
+        
+        return nil
+        
+    }
+
+
+
+
     // 5.
-   
+    
+    func replace(flavor:String, forPerson: String) -> Bool {
+        
+        
+        if favoriteFlavorsOfIceCream[forPerson] != nil {
+            
+            favoriteFlavorsOfIceCream[forPerson] = flavor
+            
+             return true
+        }
+        
+           return false
+        
+        
+        
+    }
+
     
     
     
@@ -47,7 +112,18 @@ class Icecream {
     
     // 6.
     
-    
+    func remove (person:String) -> Bool {
+        
+        if favoriteFlavorsOfIceCream[person] != nil {
+            
+            favoriteFlavorsOfIceCream[person] = nil
+            
+            return true
+        }
+        
+        return false
+        
+    }
     
     
     
@@ -56,6 +132,17 @@ class Icecream {
     
     // 7.
     
+    func numberOfAttendees() -> Int {
+        
+        var attendeesCount = 0
+        
+        for _ in favoriteFlavorsOfIceCream {
+            
+            attendeesCount += 1
+        }
+        
+        return attendeesCount
+    }
     
     
     
@@ -66,7 +153,17 @@ class Icecream {
     // 8.
     
     
-    
+    func add(person:String, withFlavor:String) -> Bool {
+        
+        if favoriteFlavorsOfIceCream[person] == nil {
+        
+        favoriteFlavorsOfIceCream[person] = withFlavor
+        
+            return true
+        }
+        
+        return false
+    }
     
     
     
@@ -75,6 +172,41 @@ class Icecream {
     
     // 9.
     
+    func attendeelist() -> String {
+        
+        var listOfAttendee = ""
+        
+        let allNames = Array(favoriteFlavorsOfIceCream.keys).sorted()
+        
+        for name in allNames {
+            
+//            let person = name
+            
+            let lastPerson = allNames.last
+            
+            let flavor = favoriteFlavorsOfIceCream[name]!
+            
+            if name == lastPerson {
+                
+                
+                
+                listOfAttendee += ("\(name) likes \(flavor)")
+            }
+
+            else  {
+                
+                listOfAttendee += ("\(name) likes \(flavor)\n")
+            }
+            
+            
+        }
+        
+        print (listOfAttendee)
+        return listOfAttendee
+    }
+    
+    
+
     
     
     
